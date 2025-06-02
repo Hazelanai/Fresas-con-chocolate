a0f12973f509d0cff80ce81097c127e329e9e195
Hazelanai <00050125@uca.edu.sv>
2025-06-02 16:04:37 -0600

    dividir

---
+++ suma de pares.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    // calcular la suma de los números pares desde 1 hasta N
+    cout <<"Ingrese el numero" << endl;
+    cin >> n;
+    int suma = 0;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 2 == 0)
+        suma += i;
+        cout << i << endl;
+
+    }
+
+    cout <<"La suma de los numeros pares de 1 hasta" << n <<"es" << suma << endl;
+
+    return 0;
+}
