e1999776bacee12a1cd39b55f02a9e95bb55a7df
Hazelanai <00050125@uca.edu.sv>
2025-06-02 17:23:07 -0600

    arreglo en codigo

--- suma_de_pares.cpp
+++ suma_de_pares.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main(){
 
     int n;
-    // calcular la suma de los números pares desde 1 hasta N
+    // calcular la suma de los números pares desde 1 hasta n
     cout <<"Ingrese el numero" << endl;
     cin >> n;
     int suma = 0;
 
     for(int i = 1; i <= n; i++){
-        if(i % 2 == 0)
-        suma += i;
-        cout << i << endl;
-
+        if(i % 2 == 0){
+            suma += i;
+            cout << i << endl;
+        }
     }
 
     cout <<"La suma de los numeros pares de 1 hasta" << n <<"es" << suma << endl;
